68a759bc77737f7ab232c88012da77183e26e0ac
maurha <habib@authscure.com.my>
2021-10-24 10:06:28 +0700

    biodata

---
+++ bahasa_pemrograman/biodata.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string input;
+    printf("BIODATA MAHASISWA\n");
+    cout << "NIM    : ";
+    getline(cin, input);
+    cout << "NAMA    : ";
+    getline(cin, input);
+    cout << "KELAS    : ";
+    getline(cin, input);
+    cout << "TTL    : ";
+    getline(cin, input);
+    cout << "ALAMAT    : ";
+    getline(cin, input);
+    cout << "HOBI    : ";
+    getline(cin, input);
+}
