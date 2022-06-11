ccb54d6f221b3540baf79955d3368cb23f7ebf64
Mabrur Habib <mabrurh@dksec.local>
2022-06-12 03:48:28 +0700

    revise

--- c++/uts2p.cpp
+++ c++/uts2p.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main() {
     string mhs[45];
-    char nilai[10];
-    float ipk[45];
-    float ip[10];
+    char predikat[10];
+    float ip[45];
+    float nilai[10];
     float hitung;
     int i=0;
     char y;
     bool isok;
 
-    printf("Menentukan Nilai IPK\n");
+    printf("Menentukan Nilai Indeks Prestasi Mahasiswa\n");
     
     for (int x=0; x<45; x++) {
         //masukkan data
@@ -20,28 +20,29 @@ int main() {
         do {
             if(i>=10) break;
             cout << "Nilai Matakuliah ke-" << i+1 << " : ";
-            cin >> nilai[i];
-            if (nilai[i] == 'A' || nilai[i] == 'a') ip[i] = 4;
-            else if (nilai[i] == 'B' || nilai[i] == 'b') ip[i] = 3;
-            else if (nilai[i] == 'C' || nilai[i] == 'c') ip[i] = 2;
-            else if (nilai[i] == 'D' || nilai[i] == 'd') ip[i] = 1;
-            else ip[i] = 0;
-            hitung += ip[i];
+            cin >> predikat[i];
+            if (predikat[i] == 'A' || predikat[i] == 'a') nilai[i] = 4;
+            else if (predikat[i] == 'B' || predikat[i] == 'b') nilai[i] = 3;
+            else if (predikat[i] == 'C' || predikat[i] == 'c') nilai[i] = 2;
+            else if (predikat[i] == 'D' || predikat[i] == 'd') nilai[i] = 1;
+            else if (predikat[i] == 'E' || predikat[i] == 'e') nilai[i] = 0;
+            else nilai[i] = 0;
+            hitung += nilai[i];
 
             cout << "Tambahkan nilai? [Y/T] ";
             cin >> y;
             switch (y){
                 case 'Y': isok = true; break;
                 case 'y': isok = true; break;
-                case 'T': ipk[x] = hitung / (i+1); isok = false; continue;
-                case 't': ipk[x] = hitung / (i+1); isok = false; continue;
+                case 'T': ip[x] = hitung / (i+1); isok = false; continue;
+                case 't': ip[x] = hitung / (i+1); isok = false; continue;
                 default : isok = true; break;
             }
             i++;
         } while(isok);
 
         //menampilkan data
-        cout << "IPK " << mhs[x] << " = " << ipk[x] <<endl;
+        cout << "Indeks Prestasi " << mhs[x] << " = " << ip[x] <<endl;
 
         //reset variable
         hitung = 0;
