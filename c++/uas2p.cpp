8c541c50da3afb8175c7f0694bec3f9ce61a9d6b
Mabrur Habib <mabrurh@dksec.local>
2022-07-12 21:38:00 +0700

    uas

--- c++/uas2p.cpp
+++ c++/uas2p.cpp
@@ -4,9 +4,9 @@ int main(){
     string nama_barang[100];
     int harga_barang[100];
     bool tambah;
-    int i=0;
     char y;
-    int total_harga;
+    int i=0;
+    int total_harga=0;
 
     printf("=== SWALAYAN SI2P ===\n");
     do {
@@ -14,19 +14,16 @@ int main(){
         cin>>nama_barang[i];
         cout<<"Harga Barang : ";
         cin>>harga_barang[i];
+        total_harga+=harga_barang[i];
 
         cout << "Masukan Barang? [Y/T] ";
         cin >> y; cout<<endl;
         switch (y){
             case 'Y': tambah = true; i++; break;
             case 'T': tambah = false; continue;
-            default : tambah = true; i++; break;
+            default : tambah = false; continue;
         }
     } while(tambah);
-    
-    for(int x=0; x<=i ; x++){
-        total_harga+=harga_barang[x];
-    }
 
-    printf("Total Harga = %d", total_harga);
+    cout<<"Total Harga = "<<total_harga;
 }
